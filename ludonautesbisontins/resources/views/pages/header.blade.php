718e40a27b326f0b065f11973d9f3c1007338b65
Unknown <quangnhanbui@gmail.com>
2017-04-05 10:32:00 +0200

    up

---
+++ ludonautesbisontins/resources/views/pages/header.blade.php
@@ -0,0 +1,67 @@
+<header>
+
+  <article class="hautheader">
+
+  <div class="container">
+      <div class="row">
+
+        <div class="col-md-2">
+
+          <a href=""><img id="home" src="{{asset('images/home.png')}}"></a>
+          <p id="premierenav">Accueil</p>
+
+        </div>
+
+        <div class="col-md-8">
+
+          <div id="logo">
+            <img id="pnj" src="{{asset('images/pnj.png')}}"><br>
+            <h1>Ludonautes<br>bisontins</h1>
+          </div>
+
+        </div>
+
+        <div class="col-md-2">
+
+          <a href=""><img id="connexion" src="{{asset('images/connexion.png')}}"></a>
+          <p id="premierenav">Connexion</p>
+
+        </div>
+
+      </div>
+    </div>
+  </article>
+
+    <img id="couverture" src="{{asset('images/couverture.jpg')}}">
+
+  <article class="nav">
+    <div class="container">
+      <div class="row">
+
+          <nav>
+              <ul>
+                  <div class="col-md-3" id="nav">
+                    <a href=""><li>Association</li></a>
+                  </div>
+
+                  <div class="col-md-3" id="nav">
+                    <a href=""><li>Jeux</li></a>
+                  </div>
+
+                  <div class="col-md-3" id="nav">
+                    <a href=""><li>Forum</li></a>
+                  </div>
+
+                  <div class="col-md-3" id="nav">
+                    <a href=""><li>Articles</li></a>
+                  </div>
+              </ul>
+          </nav>
+
+      </div>
+    </div>
+  </article>
+
+
+
+</header>
