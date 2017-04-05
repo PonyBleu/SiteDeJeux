a56afad5789089eee3d505eafe8f7af51c35a139
Unknown <quangnhanbui@gmail.com>
2017-04-05 10:42:54 +0200

    delete some files

--- ludonautesbisontins/resources/views/pages/accueil.blade.php
+++ ludonautesbisontins/resources/views/pages/accueil.blade.php
@@ -7,7 +7,7 @@
 
         <title>Laravel</title>
 
-        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="{{asset('css/accueil.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('css/header.css')}}" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Macondo|Open+Sans" rel="stylesheet">
