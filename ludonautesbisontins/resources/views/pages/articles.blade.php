8bd31cbbee903afcc58cda103c8a575aa503b3d1
Unknown <quangnhanbui@gmail.com>
2017-04-06 10:52:30 +0200

    views for the web on

--- ludonautesbisontins/resources/views/pages/articles.blade.php
+++ ludonautesbisontins/resources/views/pages/articles.blade.php
@@ -1,95 +1,125 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
+<div class="bgarticles">
+    <div class="container">
+        <div class="row">
 
-        <title>Laravel</title>
+            <div class="col-md-12 col-xs-12" id="recherche">
+                <input type="text" name="" value="Recherche" class="recherche">
+            </div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <div class="listearticles">
 
-            .full-height {
-                height: 100vh;
-            }
+            <div class="art1">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+                <div class="row">
 
-            .position-ref {
-                position: relative;
-            }
+                    <div class="col-md-8 col-xs-8">
+                        <h2>• Titre Article •</h2>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum.
+                            Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+                            lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
+                            sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+                             Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non
+                              mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent
+                              per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus
+                               condimentum
+                        </p>
+                    </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+                    <div class="col-md-4 col-xs-4">
+                        <img src="{{asset('images/taverne.jpg')}}" alt="imgarticle">
+                    </div>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </div>
+            <div class="art2">
+
+                <div class="row">
+
+                    <div class="col-md-4 col-xs-4">
+                        <img src="{{asset('images/taverne.jpg')}}" alt="imgarticle">
+                    </div>
+
+                    <div class="col-md-8 col-xs-8">
+                        <h2>• Titre Article •</h2>
+                        <p>
+                            This is Photoshop's version  of Lorem Ipsum.
+                            Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+                            lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
+                            sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+                             Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non
+                              mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent
+                              per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus
+                               condimentum
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+        <div class="art3">
+
+            <div class="row">
+
+                <div class="col-md-8 col-xs-8">
+                    <h2>• Titre Article •</h2>
+                    <p>
+                        This is Photoshop's version  of Lorem Ipsum.
+                        Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+                        lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
+                        sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+                         Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non
+                          mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent
+                          per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus
+                           condimentum
+                    </p>
+                </div>
+
+                <div class="col-md-4 col-xs-4">
+                    <img src="{{asset('images/taverne.jpg')}}" alt="imgarticle">
+                </div>
+
+            </div>
+
+        </div>
+                <div class="art4">
+
+                    <div class="row">
+
+                        <div class="col-md-4 col-xs-4">
+                            <img src="{{asset('images/taverne.jpg')}}" alt="imgarticle">
+                        </div>
+
+                        <div class="col-md-8 col-xs-8">
+                            <h2>• Titre Article •</h2>
+                            <p>
+                                This is Photoshop's version  of Lorem Ipsum.
+                                Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin,
+                                lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis
+                                sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
+                                 Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non
+                                  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent
+                                  per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus
+                                   condimentum
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+
+        </div>
+
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <p class="lienpage">1-2-3----50</p>
                 </div>
             </div>
+
         </div>
-    </body>
-</html>
+    </div>
+@endsection
--- ludonautesbisontins/resources/views/pages/inscription.blade.php
+++ ludonautesbisontins/resources/views/pages/inscription.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="text-center">
+<article class="bginscription">
+  <div class="container">
+    <div class="row">
+      <div class="text-center">
 
-        <form id="form_inscription" class="form_inscription" action="" method="post">
-          <div class="inscription col-sm-12 col-md-12 col-md-offset-2">
-              <h2 class="text-center">• Inscription •</h2>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input type="text" placeholder="Nom" class="text-center">
+          <form id="form_inscription" class="form_inscription" action="" method="post">
+            <div class="inscription col-sm-12 col-md-12 col-md-offset-2">
+                <h2 class="text-center">• Inscription •</h2>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input type="text" placeholder="Nom" class="text-center">
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input  type="text" placeholder="Prénom" class="text-center">
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input  type="text" placeholder="Pseudo" class="text-center">
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input  type="text" placeholder="E-Mail" class="text-center">
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input  type="password" placeholder="Mot de passe" class="text-center">
+              </div>
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                <input  type="password" placeholder="Confirmation de mot de passe" class="text-center">
+              </div>
+              <div class="col-sm-3 col-sm-offset-9 col-md-3 col-lg-3">
+                <button type="button" name="button">Envoyer</button>
+              </div>
             </div>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input  type="text" placeholder="Prénom" class="text-center">
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input  type="text" placeholder="Pseudo" class="text-center">
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input  type="text" placeholder="E-Mail" class="text-center">
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input  type="password" placeholder="Mot de passe" class="text-center">
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-12">
-              <input  type="password" placeholder="Confirmation de mot de passe" class="text-center">
-            </div>
-            <div class="col-sm-2 col-sm-offset-10 col-md-2 col-lg-2">
-              <button type="button" name="button">Envoyer</button>
-            </div>
-          </div>
 
-        </form>
+          </form>
 
+      </div>
     </div>
   </div>
-</div>
+</article>
 @endsection
